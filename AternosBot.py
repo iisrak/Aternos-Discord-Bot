0f986984c1532247379405c270bc797fecb881f0
LaxionDev <61939474+LaxionDev@users.noreply.github.com>
2021-09-10 07:59:24 +0100

    Update AternosBot.py

--- AternosBot.py
+++ AternosBot.py
@@ -2,9 +2,9 @@ from aternosapi import AternosAPI
 import discord
 from discord.ext import commands, tasks
 
-headers_cookie = "Headers Cookie"
-TOKEN = "Token"
-server = AternosAPI(headers_cookie, TOKEN)
+headers_cookie = "COOKIE"
+TOKEN = "TOKEN"
+server = AternosAPI(headers_cookie, TOKEN, timeout=10)
 
 client = commands.Bot(command_prefix = ["-"])
 client.remove_command('help')
@@ -26,17 +26,22 @@ async def on_ready():
 
 @client.command()
 async def help(ctx):
-    embed = discord.Embed(title="Current Commands", description="A description", color=0x00ff00)
+    embed = discord.Embed(title="Current Commands", description="By Laxion", color=0x00ff00)
     embed.add_field(name="start", value="Starts the server.", inline=True)
     embed.add_field(name="stop", value="Stops the server.", inline=True)
     embed.add_field(name="status", value="Shows the current status of the server.", inline=True)
     embed.add_field(name="info", value="Shows server information.", inline=True)
+    embed.add_field(name="players", value="Shows players information.", inline=True)
     await ctx.send(embed = embed)
 
 @client.command()
 async def start(ctx):
-    await ctx.send("Starting...")
-    await cmd("start", ctx)
+    status = str(server.GetStatus())
+    if (status == "Offline"):
+        await ctx.send("Starting...")
+        await cmd("start", ctx)
+    else:
+        await ctx.send("Server is already starting/started!")
 
 @client.command()
 async def stop(ctx):
@@ -50,5 +55,14 @@ async def info(ctx):
 @client.command()
 async def status(ctx):
     await cmd("status", ctx)
+
+@client.command()
+async def players(ctx):
+    z = server.GetPlayerInfo()
+    if (len(z) == 0):
+        await ctx.send("No players online.")
+        return
+    for i in range(len(z)):
+        await ctx.send(z[i])
         
-client.run("Token")
+client.run("DISCORD BOT TOKEN")
